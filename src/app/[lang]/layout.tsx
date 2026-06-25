51866692c8bde4393a4095f2d0df87a30f0d6396
Lironn Levy <lironnlevy@Lironns-MacBook-Air.local>
2026-06-25 11:48:48 +0300

    Add Google Tag Manager (GTM-MDZLRWHN)

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { LANGS, dir, type Lang } from "@/lib/i18n";
 import SiteEffects from "@/components/SiteEffects";
 import "../globals.css";
@@ -32,7 +33,20 @@ export default function LangLayout({
 }) {
   return (
     <html lang={params.lang} dir={dir(params.lang)}>
+      <head>
+        <Script id="gtm-head" strategy="afterInteractive">{`
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MDZLRWHN');
+        `}</Script>
+      </head>
       <body>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDZLRWHN"
+            height="0" width="0" style={{display:"none",visibility:"hidden"}} />
+        </noscript>
         <SiteEffects />
         {children}
       </body>
